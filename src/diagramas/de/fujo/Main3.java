be3742a775a4331ea59e53223dde0895b8116d20
EliteBook <EliteBook@DESKTOP-U1QVONF>
2021-07-09 19:40:23 -0500

    Tabla del 7

---
+++ src/diagramas/de/fujo/Main3.java
@@ -0,0 +1,20 @@
+package diagramas.de.fujo;
+
+import java.util.Scanner;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado =new Scanner(System.in);//TECLADO
+		int i=7;
+		int j=0;
+		int k;
+		for (j=0;j<=12;j=j+1) {
+			k=i*j;
+		
+			System.out.println(i+ "*"+j+"="+k);
+			}
+	}
+
+}
