8061f7ee192fada08d055a3b4461bbdafb9df9b5
usuario <usuario@DESKTOP-3RHDJ3H>
2021-07-16 19:28:59 -0500

    Comentario

--- src/diagramas/de/fujo/Mainfigurainvertida.java
+++ src/diagramas/de/fujo/Mainfigurainvertida.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Mainfigurainvertida {
 
+	//Buen Trabajo :)
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Ingrese el número de filas: ");
