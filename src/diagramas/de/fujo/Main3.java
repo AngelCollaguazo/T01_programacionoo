a43d08329072ba68d1776c1a7d11baaa19e7819a
EliteBook <EliteBook@DESKTOP-U1QVONF>
2021-07-21 19:00:18 -0500

    Tipos de datos

--- src/diagramas/de/fujo/Main3.java
+++ src/diagramas/de/fujo/Main3.java
@@ -3,14 +3,14 @@ package diagramas.de.fujo;
 import java.util.Scanner;
 
 public class Main3 {
-//Imprimir la tabla del 7 multiplicado hasta el 12
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado =new Scanner(System.in);//TECLADO
 		int i=7;
 		int j=0;
 		int k;
-		for (j=0;j<=12;j++) {
+		for (j=0;j<=12;j=j+1) {
 			k=i*j;
 		
 			System.out.println(i+ "*"+j+"="+k);
--- src/diagramas/de/fujo/Mainfigurainvertida.java
+++ src/diagramas/de/fujo/Mainfigurainvertida.java
@@ -7,13 +7,17 @@ public class Mainfigurainvertida {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Ingrese el número de filas: ");
+
 		Scanner teclado = new Scanner(System.in);// TECLADO
 		int n = teclado.nextInt();
-		for (int i = n; i > 0; i--) {
-			for (int j = 1; j < i + 1; j++) {
+
+		for (int i = n; i>0; i--) {
+			for (int j = 1; j<i+1; j++) {
 				System.out.print("*");
 			}
-			System.out.println();// Salto de linea
+			System.out.println();//Salto de linea
 		}
+
 	}
+
 }
