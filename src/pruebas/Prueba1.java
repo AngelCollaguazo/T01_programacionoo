67fbee2f3d8595ce39966284dc81dcaad40127b4
EliteBook <EliteBook@DESKTOP-U1QVONF>
2021-07-30 14:05:56 -0500

    Ciclo While y Do While

---
+++ src/pruebas/Prueba1.java
@@ -0,0 +1,39 @@
+package pruebas;
+
+import java.util.Scanner;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Crear un programa que solicite ingresar un número (solo positivos), y así
+		 * seguirá solicitando ingresar un número hasta que se ingrese el número cero
+		 * (0), cuando se ingrese el numero cero, ya no solicitará más números y en ese
+		 * momento el programa debe imprimir la suma solo de los números ingresados
+		 * hasta ese momento. Adicional debera imrpimir la cantidad de numeros
+		 * ingresados por el usuario Con Bucle While
+		 */
+		Scanner teclado = new Scanner(System.in);// TECLADO
+		float s = 0;
+		float p = 0;
+		int c = 0;
+		float i = 1;
+		while (i != 0) {
+			System.out.println("Ingrese un numero positivo");
+			int n = teclado.nextInt();
+			if (n == 0) {
+				i = -1;
+			} else {
+				s = s + n;
+				p = s / i;
+				c = c + 1;
+			}
+			i++;
+		}
+		System.out.println("La suma es: " + s);
+		System.out.println("El promedio es: " + p);
+		System.out.println("La cantidad de numeros ingresados es: " + c);
+	}
+}
+
---
+++ src/pruebas/Prueba2.java
@@ -0,0 +1,42 @@
+package pruebas;
+
+import java.util.Scanner;
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Crear un programa que solicite ingresar un número (solo positivos), y así
+		 * seguirá solicitando ingresar un número hasta que se ingrese el número (-1),
+		 * cuando se ingrese el numero -1, ya no solicitará más números y en ese momento
+		 * el programa debe imprimir la suma solo de los números pares ingresados y el
+		 * promedio solo de los números impares ingresados hasta ese momento Con Bucle
+		 * Do While
+		 */
+		Scanner teclado = new Scanner(System.in);// TECLADO
+		int sp = 0;
+		float si = 0;
+		float pi = 0;
+		int ci = 0;
+		int i = 1;
+		do {
+			System.out.println("Ingrese un numero positivo");
+			int n = teclado.nextInt();
+			if (n == -1) {
+				i = -2;
+			} else if (n % 2 == 0) {
+				sp = sp + n;
+			} else {
+				si = si + n;
+				ci = ci + 1;
+				pi = si / ci;
+			}
+			i++;
+		} while (i != -1);
+		System.out.println("La suma de los pares es: " + sp);
+		System.out.println("El promedio de los impares es: " + pi);
+		System.out.println("La cantidad de numeros impares ingresados es: " + ci);
+	}
+
+}
