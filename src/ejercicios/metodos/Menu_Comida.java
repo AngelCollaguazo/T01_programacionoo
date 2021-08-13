ad127d268a43fac20d93a301278e36c28e0146e4
AngelCollaguazo <86030526+AngelCollaguazo@users.noreply.github.com>
2021-08-13 13:58:25 -0500

    Add files via upload
    metodos entre clases

---
+++ src/ejercicios/metodos/Menu_Comida.java
@@ -0,0 +1,13 @@
+package restaurante;
+
+
+public class Menu_Comida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Turno impresion = new Turno();//instanciar la clase
+		impresion.imprimir("Turno para ","Angel Collaguazo"  , "1776348723" , "2089748", 00001, 12, "Caldo de bolas de verde", "Filete de pollo apananizado", "Pudin de manzana");
+	}
+
+}
