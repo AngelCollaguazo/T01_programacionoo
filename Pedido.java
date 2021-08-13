ca99326e1bd6f6147d5656349f70dc4a75e479ef
AngelCollaguazo <86030526+AngelCollaguazo@users.noreply.github.com>
2021-08-13 13:50:37 -0500

    Add files via upload
    metodos

---
+++ Pedido.java
@@ -0,0 +1,15 @@
+package restaurante;
+
+
+public class Pedido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Turno impresion1 = new Turno();//instanciar la clase
+		impresion1.imprimir( "Turno para ","Angel Collaguazo" , "1776348723" , "2089748", 00001, 12, "Caldo de bolas de verde", "Filete de pollo apananizado", "Pudin de manzana");
+
+	}
+
+}
