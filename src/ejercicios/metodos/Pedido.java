5189e0711651b3e01e5a59777f198d46d519fad2
AngelCollaguazo <86030526+AngelCollaguazo@users.noreply.github.com>
2021-08-13 14:33:49 -0500

    Add files via upload
    trabajo parejas

---
+++ src/ejercicios/metodos/Pedido.java
@@ -0,0 +1,13 @@
+package metodo.restaurante;
+
+import tareaa.pareja.TurnoOdontologia;
+
+public class Pedido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Turno Turno1 = new Turno();
+		Turno1.Imprimir("Pedido a", "Dayvi Pilataxi", "17625723129", "2054673", "Filete de pescado apanado", "00001");
+	}
+
+}
